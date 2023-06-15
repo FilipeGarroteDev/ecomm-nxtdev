9f8b0946fcfdd422dd6dfb93fcd1b598a57581b5
FilipeGarroteDev <filipe.garrote@gmail.com>
2023-06-15 12:29:43 -0300

    refactor: Node.js: Listar as Categorias - Refatoração dos arquivos cli.js e CategoryService.js para manutenção do código limpo e inserção de script de execução

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -1,11 +1,27 @@
+import chalk from "chalk";
+
+function handleErrors(err) {
+	switch (err) {
+		case "ECONNREFUSED":
+			return chalk.redBright(
+				"O link informado se encontra offline. Tente novamente mais tarde."
+			);
+		case "ENOTFOUND":
+			return chalk.redBright("O link informado não existe. Refaça a operação.");
+		default:
+			return chalk.redBright(
+				`Não foi possível completar a operação. Código do erro: ${err}`
+			);
+	}
+}
 export default class CategoryService {
 	static async findCategories() {
 		try {
 			const response = await fetch("http://localhost:3000/categories");
-			console.log(`response status: ${response.status}`);
+			console.log(chalk.bgGreen(`response status: ${response.status}`));
 			return response.json();
 		} catch (error) {
-			console.log("OIII");
+			return handleErrors(error.cause.code);
 		}
 	}
 }
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,16 +1,22 @@
 import CategoryService from "./CategoryService.js";
+import chalk from "chalk";
 
-const cliArgs = process.argv
+const cliArgs = process.argv;
 
 async function processarComando(args) {
+	const flag = args[2];
 
-	switch (args[2]) {
+	switch (flag) {
 		case "--listarCategorias":
 			const categoriesList = await CategoryService.findCategories();
 			console.log(categoriesList);
 			return;
 		default:
-			console.error("Comando não reconhecido, refaça a operação.");
+			console.error(
+				chalk.black.bgBlackBright(
+					"Comando não reconhecido, refaça a operação.\n\nComandos válidos:\n--listarCategorias: Lista as categorias existentes;"
+				)
+			);
 	}
 }
 
