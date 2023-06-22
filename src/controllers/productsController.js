61fab96a0c5aaac234e4ab31ac1a2fb52f7b7f16
FilipeGarroteDev <filipe.garrote@gmail.com>
2023-06-22 12:37:02 -0300

    feat: Implementação do CRUD de Produtos - Implementado metódo PUT para atualização de um produto específico

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -38,26 +38,29 @@ class ProductsController {
     }
   }
 
-  // static async updateCategory(req, res) {
-  //   const { id } = req.params;
-  //   const newCategoryData = req.body;
+  static async updateProduct(req, res) {
+    const { id } = req.params;
+    const newProductData = req.body;
 
-  //   try {
-  //     const category = await CategoriesModel.findByIdAndUpdate(id, { $set: newCategoryData }, { runValidators: true });
+    try {
+      const product = await ProductsModel.findByIdAndUpdate(id, { $set: newProductData }, { runValidators: true });
 
-  //     if (!category) return res.status(404).send('Não há quaisquer categorias com o id informado. Por gentileza, refaça a operação.');
+      if (!product) return res.status(404).send('Não há quaisquer produtos com o id informado. Por gentileza, refaça a operação.');
 
-  //     return res.status(200).send('Categoria atualizada com sucesso');
-  //   } catch (error) {
-  //     if (error.name === 'CastError') {
-  //       return res.status(400).send('O id informado é inválido, favor informe um id compatível com o tipo ObjectID');
-  //     }
-  //     if (error.name === 'ValidationError') {
-  //       return res.status(422).send(error.message);
-  //     }
-  //     return res.status(500).send('Houve um erro com sua requisição. Por favor, tente novamente');
-  //   }
-  // }
+      return res.status(200).send('Produto atualizado com sucesso');
+    } catch (error) {
+      if (error.name === 'CastError' && (error.path === 'precoUnitario' || error.path === 'quantidadeEmEstoque')) {
+        return res.status(400).send('O valor dos campos "quantidadeEmEstoque" e "precoUnitario" devem ser do tipo "Number"');
+      }
+      if (error.name === 'CastError' && error.path === '_id') {
+        return res.status(400).send('O id informado é inválido, favor informe um id compatível com o tipo ObjectID');
+      }
+      if (error.name === 'ValidationError') {
+        return res.status(422).send(error.message);
+      }
+      return res.status(500).send('Houve um erro com sua requisição. Por favor, tente novamente');
+    }
+  }
 
   // static async deleteCategory(req, res) {
   //   const { id } = req.params;
