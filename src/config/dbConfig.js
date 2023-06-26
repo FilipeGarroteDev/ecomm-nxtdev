0ce88238569ba1b8c4241a3f3c1639ed2f7a9e7e
FilipeGarroteDev <filipe.garrote@gmail.com>
2023-06-26 00:31:11 -0300

    feat: Docker: Executar a API pelo Docker

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
 const database = process.argv[6] === '--test' ? 'ecomm-test' : 'ecomm';
-mongoose.connect(`mongodb://admin:secret@localhost:27017/ecom${database}?authSource=admin`);
+const host = process.env.HOST_MONGO ? process.env.HOST_MONGO : 'localhost';
+console.log(host);
+mongoose.connect(`mongodb://admin:secret@${host}:27017/ecom${database}?authSource=admin`);
 
 const db = mongoose.connection;
 
